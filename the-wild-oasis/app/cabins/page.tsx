b7996ec8c34fba4d6afec3e333182fcb64aeaecc
ajdin <ajdin.work@gmail.com>
2024-06-05 17:22:45 +0200

    wild oasis setup (Routes)

--- the-wild-oasis/app/cabins/page.tsx
+++ the-wild-oasis/app/cabins/page.tsx
@@ -1,22 +1,34 @@
-import Counter from "../components/Counter";
-import Navigation from "../components/Navigation";
-import { User } from "../types/UserType";
+import CabinCard from "../_components/CabinCard";
 
-export default async function Cabins() {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  const data: User[] = await res.json();
+export const metadata = {
+  title: "Cabins",
+};
 
-  console.log(data);
+export default function Page() {
+  // CHANGE
+  const cabins = [];
 
   return (
-    <>
-      <h1>Cabins page</h1>;
-      <ul>
-        {data.map((e) => (
-          <li key={e.id}>{e.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
-    </>
+    <div>
+      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-primary-200 text-lg mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+
+      {cabins.length > 0 && (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin) => (
+            <CabinCard cabin={cabin} key={cabin.id} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
--- the-wild-oasis/app/layout.tsx
+++ the-wild-oasis/app/layout.tsx
@@ -1,8 +1,23 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Header from "./_components/Header";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: "The wild Oasis",
+  //title: "The wild Oasis",
+  title: {
+    template: "%s - The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({
@@ -12,12 +27,14 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        {children}
+      <body
+        className={` antialiased text-primary-100 min-h-screen bg-primary-950 ${josefin.className} flex flex-col `}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12   grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
